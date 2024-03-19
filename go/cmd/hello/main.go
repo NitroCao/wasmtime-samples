c6c5e2d15b0456dd75c38260bdb2ddba3f36560a
NitroCao <17915615+NitroCao@users.noreply.github.com>
2024-03-19 16:14:17 +0800

    Hello wasm (#1)
    * feat(wasm): create skeleton of assemblyscript
    
    * feat(go): call wasm functions in Go

---
+++ go/cmd/hello/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/bytecodealliance/wasmtime-go/v18"
+)
+
+var (
+	wasmFile string
+)
+
+func init() {
+	flag.StringVar(&wasmFile, "wasm", "", "wasm file")
+}
+
+func main() {
+	flag.Parse()
+
+	store := wasmtime.NewStore(wasmtime.NewEngine())
+	wasm, err := os.ReadFile(wasmFile)
+	if err != nil {
+		log.Fatalf("failed to read wasm %s: %s", wasmFile, err)
+	}
+
+	module, err := wasmtime.NewModule(store.Engine, wasm)
+	if err != nil {
+		log.Fatalf("failed to create module: %s", err)
+	}
+
+	instance, err := wasmtime.NewInstance(store, module, []wasmtime.AsExtern{})
+	if err != nil {
+		log.Fatalf("failed to create instance: %s", err)
+	}
+
+	run := instance.GetFunc(store, "add")
+	if run == nil {
+		log.Printf("not a function")
+	}
+
+	if result, err := run.Call(store, 2, 3); err != nil {
+		log.Fatalf("failed to run: %s", err)
+	} else {
+		log.Printf("result: %d", result)
+	}
+}
---
+++ go/go.mod
@@ -0,0 +1,5 @@
+module wasmgo
+
+go 1.21.6
+
+require github.com/bytecodealliance/wasmtime-go/v18 v18.0.0 // indirect
