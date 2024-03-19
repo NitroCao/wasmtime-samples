535f61e0de989611dcf1b8576e8e06a0cc1b2bab
Nitro Cao <jaycecao520@gmail.com>
2024-03-19 16:23:37 +0800

    feat: import anonymous functions from Go

--- wasm-modules/assembly/index.ts
+++ wasm-modules/assembly/index.ts
@@ -1,5 +1,9 @@
 // The entry file of your WebAssembly module.
 
+@external("", "")
+declare function doDouble(n: i32): i32
+
 export function add(a: i32, b: i32): i32 {
+  a = doDouble(a);
   return a + b;
 }
